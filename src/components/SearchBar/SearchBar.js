afb2f0ce4fdb48944f8f86b964c022abe5dc8b2a
Adrian Zablocki <adrian@MacBook-Pro-Adrian.local>
2018-08-26 13:36:22 +0200

    Fix submit form

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classes from './SearchBar.css';
 
 const searchBar = (props) => (
-  <div className={classes.SearchBar} onSubmit={props.submited}>
+  <form className={classes.SearchBar} onSubmit={props.submited}>
     <input className={classes.SearchInput} type="text" placeholder="Who are you looking for?" onChange={props.changed} />
     <input className={classes.SearchSubmit} type="submit" value="Search" />
-  </div>
+  </form>
 );
 
 export default searchBar;
